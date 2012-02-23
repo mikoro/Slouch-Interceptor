0fd631a881bba41e3e1b3d0fa19b1d968247e913
Mikko Ronkainen <firstname@mikkoronkainen.com>
2012-02-23 22:41:36 +0200

    Added graphics and overlay window

--- SlouchInterceptor/MainForm.Designer.cs
+++ SlouchInterceptor/MainForm.Designer.cs
@@ -28,22 +28,82 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+			this.pictureBox = new System.Windows.Forms.PictureBox();
+			this.contextMenuStripNotifyIcon = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+			this.contextMenuStripNotifyIcon.SuspendLayout();
 			this.SuspendLayout();
+			// 
+			// notifyIcon
+			// 
+			this.notifyIcon.ContextMenuStrip = this.contextMenuStripNotifyIcon;
+			this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+			this.notifyIcon.Text = "Slouch Interceptor";
+			this.notifyIcon.Visible = true;
+			this.notifyIcon.Click += new System.EventHandler(this.NotifyIconClick);
+			// 
+			// pictureBox
+			// 
+			this.pictureBox.ErrorImage = null;
+			this.pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox.Image")));
+			this.pictureBox.InitialImage = null;
+			this.pictureBox.Location = new System.Drawing.Point(195, 154);
+			this.pictureBox.Name = "pictureBox";
+			this.pictureBox.Size = new System.Drawing.Size(396, 498);
+			this.pictureBox.TabIndex = 0;
+			this.pictureBox.TabStop = false;
+			// 
+			// contextMenuStripNotifyIcon
+			// 
+			this.contextMenuStripNotifyIcon.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+			this.contextMenuStripNotifyIcon.Name = "contextMenuStripNotifyIcon";
+			this.contextMenuStripNotifyIcon.Size = new System.Drawing.Size(93, 26);
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+			this.exitToolStripMenuItem.Text = "Exit";
+			this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItemClick);
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(408, 305);
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(800, 800);
+			this.ControlBox = false;
+			this.Controls.Add(this.pictureBox);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
 			this.Name = "MainForm";
+			this.Opacity = 0.9D;
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Slouch Interceptor configuration";
-			this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+			this.TopMost = true;
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+			this.contextMenuStripNotifyIcon.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.NotifyIcon notifyIcon;
+		private System.Windows.Forms.PictureBox pictureBox;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStripNotifyIcon;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
 	}
 }
 

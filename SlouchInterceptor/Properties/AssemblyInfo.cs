048400ecf87e79d97b91bc1015d98023523368a7
Mikko Ronkainen <mikko@mikkoronkainen.com>
2012-02-23 21:45:40 +0200

    Initial commit

---
+++ SlouchInterceptor/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Slouch Interceptor")]
+[assembly: AssemblyDescription("Intercepts your slouchiness")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mironworks")]
+[assembly: AssemblyProduct("Slouch Interceptor")]
+[assembly: AssemblyCopyright("Copyright © 2012 Mironworks")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d2dbd870-9b6e-4cc1-9584-7c7d35b94693")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
